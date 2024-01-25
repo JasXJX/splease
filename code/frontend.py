35d36b652ef13fb675713da9a58686ed70dae12b
Jason Xu <55168153+JasXJX@users.noreply.github.com>
2024-01-25 16:36:46 -0500

    create draft for build receipt

--- code/frontend.py
+++ code/frontend.py
@@ -1,10 +1,31 @@
 from util import Receipt
 
-class prompter(object):
+
+class Prompter(object):
     def __init__(self):
-        self.receipt = Receipt()
+        # self.receipt = Receipt()
         self.welcome = "Welcome to Splease, a bill splitting app!"
 
     # method that greets the user and asks what they wanna do next
     def begin(self):
         print(self.welcome)
+
+    def build_receipt(self):
+        receipt = Receipt()
+        response = "Enter \"Done!\" when you\'re finished entering items " + \
+            "or \"del\" to delete the last entry."
+        print(response)
+        while response.casefold() != "done!":
+            print(receipt.print())
+            item = input("Please enter item name:")
+            if item.casefold() != "done!":
+                price = input("Please enter item price:")
+                person = input("Please enter who this item is for:")
+                receipt.add_item(item, price, person)
+        response = "no"
+        while response == "no":
+            tips = input("How much was the tips?")
+            taxes = input("And the taxes?")
+            response = input("Does this look good?\n" +
+                             "Tips: {tips}\nTaxes: {taxes}")
+        receipt.add_tip_n_tax(tips, taxes)
